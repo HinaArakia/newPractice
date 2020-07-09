54722c3491f3bca1aa066e7c5a0940624bd52268
Hina Araki <h.araki.aforce@gmail.com>
2020-07-09 13:54:32 +0900

    refs #1: git add　を試すテスト

---
+++ practice_test_2.py
@@ -0,0 +1,12 @@
+def get_fruit_price(fruit_id, location_id):
+    """
+    果物の値段を取得する。
+
+    Parameters
+    ----------
+    fruit_id : int
+        対象の果物のマスタID。
+    location_id : int
+        対象地域のマスタID。
+    """
+    pass
